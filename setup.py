22003db7c810fbfd46ea060b8145835f74ca6d33
Nina Raymond <norfazlinahamdan@gmail.com>
2022-01-05 14:46:09 +1100

    Update

--- setup.py
+++ setup.py
@@ -1,10 +1,37 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name='src',
-    packages=find_packages(),
-    version='0.1.0',
-    description='ALA automation',
-    author='Nor Raymond',
-    license='MIT',
+with open('README.md', encoding = 'utf-8') as f:
+    long_description = f.read()
+
+package_nam_var = 'RandomForest_Flask'
+
+setuptools.setup(
+    name = package_nam_var,
+    version = '0.0.1',
+    description = 'Machine Learning production code skeleton',
+    long_description = long_description,
+    long_description_content_type = 'text/markdown',
+    author = 'Nor Raymond',
+    author_email = 'norfazlinahamdan@gmail.com',
+    url="https://github.com/girlrilaz/production_template_ml",
+    project_urls={
+        "Bug Tracker": "https://github.com/girlrilaz/production_template_ml/issues",
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+    package_dir={"": "src"},
+    packages=setuptools.find_packages("src"),
+    python_requires = '>=3.8, <4',
+    install_requires = ["pandas>=0.23.1",
+                        "scikit-learn>=0.21.0",
+                        "joblib",
+                        "click",
+                        "flask",
+                    ],
+    extras_require = {'dev': ["pep517", "pytest", "pylint", "black", "pre-commit"], 
+    }, 
+    entry_points={"console_scripts": [package_nam_var + "=" + package_nam_var + ".cli:cli"]},
 )
