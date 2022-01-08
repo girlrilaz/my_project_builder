291e89956a4e3158f0bd601d271996d71a821b30
Nina Raymond <norfazlinahamdan@gmail.com>
2022-01-08 18:01:39 +1100

    Update

--- src/skeleton/executor/model_trainer.py
+++ src/skeleton/executor/model_trainer.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
+"""Model Training Executor"""
 
 # standard library
-import os, re
+import os
+import re
 import pickle
 
+#external
 from xgboost.sklearn import XGBClassifier
+from sklearn.model_selection import GridSearchCV, StratifiedKFold
 
 #internal
 from utils.logger import get_logger
 
-#external
-from sklearn.model_selection import GridSearchCV, StratifiedKFold
-
-
 LOG = get_logger('xgboost_training')
 
 class ModelTrainer:
+    """Model training executor class"""
 
     def __init__(self, model, name, folder, version, X_train, y_train, init_params):
         self.model = model
@@ -36,30 +37,30 @@ class ModelTrainer:
         Save pickle models into saved_models
         '''
 
-        LOG.info(f'Start model training .....')
-        LOG.info(f'..... grid searching')
+        LOG.info('Start model training .....')
+        LOG.info('..... grid searching')
 
         grid_params =  {
             "nthread":[4],
-            "booster": ["gbtree"], 
+            "booster": ["gbtree"],
             "n_estimators": [20, 40],
-            "objective": ["binary:logistic"], 
+            "objective": ["binary:logistic"],
             "learning_rate" : [0.25, 0.5],
-            "eval_metric": ["error"], 
-            "eta": [0.3], 
-            "gamma": [0], 
-            "max_depth": [6], 
-            "min_child_weight": [4], 
-            "max_delta_step": [0], 
-            "subsample": [1], 
-            "colsample_bytree": [1], 
-            "seed": [0], 
+            "eval_metric": ["error"],
+            "eta": [0.3],
+            "gamma": [0],
+            "max_depth": [6],
+            "min_child_weight": [4],
+            "max_delta_step": [0],
+            "subsample": [1],
+            "colsample_bytree": [1],
+            "seed": [0],
             "scale_pos_weight": [1]
-        } 
-    
+        }
+
         # grid = GridSearchCV(self.model, param_grid=grid_params, cv=10, n_jobs=1)
-        grid =  GridSearchCV(self.model, param_grid=grid_params, n_jobs=5, 
-                   cv=StratifiedKFold(n_splits=5, random_state=0, shuffle=True), 
+        grid =  GridSearchCV(self.model, param_grid=grid_params, n_jobs=5,
+                   cv=StratifiedKFold(n_splits=5, random_state=0, shuffle=True),
                    scoring='roc_auc',
                    verbose=2, refit=True)
 
@@ -78,82 +79,9 @@ class ModelTrainer:
         LOG.info(f"saving model: {self.name + '_' + self.folder + '.' + self.version}")
 
         save_path = os.path.join(self.model_save_path, self.name, self.folder)
-        os.makedirs(save_path, exist_ok = True) 
-        pickle.dump(final_model, open(os.path.join(save_path, self.name + '_' + self.folder + '.' + self.version + '.pickle'),'wb'))
+        os.makedirs(save_path, exist_ok = True)
+        pickle.dump(final_model, open(os.path.join(save_path, self.name + '_' + \
+            self.folder + '.' + self.version + '.pickle'),'wb'))
 
         LOG.info(f"saved model: {save_path}")
-        LOG.info(f"Model training completed")
-
-    def _write_summary(self, loss, epoch):
-
-        '''
-        Write training summary (console print and logs folder)
-        '''
-        print('')
-        # with self.train_summary_writer.as_default():
-        #     tf.summary.scalar('loss', loss, step=epoch)
-        #     tf.summary.scalar('accuracy', self.metric.result(), step=epoch)
-
-
-
-## MODEL PREDICT / EVALUATION
-
-    # def create_y_weights(self, w, y):
-    #     """
-    #     helper routine to create  weight of size y .All the values are same i.e w
-    #     """
-    #     r = y.copy()
-    #     r[r == 1] = w  
-    #     r[r == 0] = 1
-    #     return r 
-
-    # def model_cross_validation(self, name_model, X, y, weight=1, fold=3):
-    #     """
-    #     utility function to find the best model using k fold cross validation
-        
-    #     arguements: 
-    #     name_model -- name model
-    #     X -- transformed feature dataset
-    #     y -- corresponding target dataset
-    #     weight -- weight to apply to positive sample. Default to 1 and we will see how it can be used with different weights.
-        
-    #     return: cross validated model with k == 3. It will return prob as well as prediction
-    #     """
-        
-    #     # get the weights of w of size y
-    #     y_weights = self.create_y_weights(weight, y)
-        
-    #     # get prediction
-    #     y_proba = cross_val_predict(
-    #                 name_model, X, y, cv=fold, method="predict_proba",
-    #                 fit_params={'eval_metric':'auc',
-    #                         'sample_weight':y_weights})
-        
-    #     # get probabilies
-    #     y_pred = cross_val_predict(
-    #                 name_model, X, y, cv=fold, method="predict",
-    #                 fit_params={'eval_metric':'auc',
-    #                         'sample_weight':y_weights})
-        
-    #     # result
-    #     cv_result = {
-    #         'model': name_model,
-    #         'X':X,
-    #         'y':y,
-    #         'weight': weight,
-    #         'proba':y_proba,
-    #         'pred':y_pred,
-    #     }
-        
-    #     return cv_result
-
-    # def model_pred(self, X_train, y_train):
-
-    #     xgb_model = self.model
-
-    #     # use cross validation to find best model
-    #     xgb_predict_obj = self.model_cross_validation(xgb_model, X_train, y_train, weight=1)
-
-    #     xgb_cls_1_proba = xgb_predict_obj['proba'][:,1]
-    #     xgb_y_pred = xgb_predict_obj['pred']
-
+        LOG.info("Model training completed")
--- src/skeleton/models/model.py
+++ src/skeleton/models/model.py
@@ -1,23 +1,24 @@
 # -*- coding: utf-8 -*-
-"""Unet model"""
+"""XGboost model"""
 
 # standard library
-import os, sys
-import pandas as pd
-import pickle
-sys.path.append('.')
-
-# internal
+import os
+import sys
 from .base_model import BaseModel
-from utils.dataloader import DataLoader
-from utils.logger import get_logger
-from executor.model_trainer import ModelTrainer
+import pickle
+import pandas as pd
+sys.path.append('.')
 
 # external
 from xgboost import XGBClassifier
 from sklearn.metrics import accuracy_score
-from sklearn.model_selection import KFold, StratifiedKFold
 from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import KFold, StratifiedKFold
+
+# internal
+from utils.logger import get_logger
+from utils.dataloader import DataLoader
+from executor.model_trainer import ModelTrainer
 
 LOG = get_logger('xgboost')
 
@@ -34,6 +35,10 @@ class ModelName(BaseModel):
         self.model = None
         self.X_pipeline = []
         self.y_pipeline = []
+        self.X_train = None
+        self.y_train = None
+        self.X_test = None
+        self.y_test = None
         self.train_dataset = []
         self.test_dataset = []
         self.numerical = self.config.data.numerical_att
@@ -54,24 +59,28 @@ class ModelName(BaseModel):
 
         self.dataset = DataLoader().load_data(self.config.data)
 
-        LOG.info(f"..... validating all data")
+        LOG.info("..... validating all data")
 
         try:
             validate = DataLoader().validate_schema(self.dataset)
             if validate is None:
-                LOG.info(f"PASS: data validation passed.")
+                LOG.info("PASS: data validation passed.")
         except:
-            LOG.error(f"FAIL: data validation failed.")
-            raise Exception("ERROR - FAIL:(dataloader) - invalid data schema") 
+            LOG.error("FAIL: data validation failed.")
+            raise Exception("ERROR - FAIL:(dataloader) - invalid data schema")
             # sys.exit(100) # exit if using log and no raise exception
 
         self.train_dataset, self.test_dataset = DataLoader().preprocess_data(self.dataset, self.test_size, self.random_state)
 
-        self.X_train= DataLoader().feature_pipeline(self.numerical, self.categorical).fit(self.train_dataset).transform(self.train_dataset)
-        self.y_train = DataLoader().target_pipeline(self.target).fit(self.train_dataset[self.target]).transform(self.train_dataset[self.target])
+        self.X_train= DataLoader().feature_pipeline(self.numerical, self.categorical) \
+            .fit(self.train_dataset).transform(self.train_dataset)
+        self.y_train = DataLoader().target_pipeline(self.target).fit(self.train_dataset[self.target]) \
+            .transform(self.train_dataset[self.target])
 
-        self.X_test= DataLoader().feature_pipeline(self.numerical, self.categorical).fit(self.test_dataset).transform(self.test_dataset)
-        self.y_test = DataLoader().target_pipeline(self.target).fit(self.test_dataset[self.target]).transform(self.test_dataset[self.target])
+        self.X_test= DataLoader().feature_pipeline(self.numerical, self.categorical).fit(self.test_dataset) \
+            .transform(self.test_dataset)
+        self.y_test = DataLoader().target_pipeline(self.target).fit(self.test_dataset[self.target]) \
+            .transform(self.test_dataset[self.target])
 
     def build(self):
 
@@ -87,25 +96,25 @@ class ModelName(BaseModel):
 
         """Compiles and trains the model with train dataset"""
 
-        trainer = ModelTrainer(self.model, self.model_name, self.model_folder, self.model_version, 
+        trainer = ModelTrainer(self.model, self.model_name, self.model_folder, self.model_version,
                                 self.X_train, self.y_train, vars(self.model_params))
         trainer.train()
 
-    def evaluate(self): 
+    def evaluate(self):
 
         """Predicts results for the test dataset"""
 
-        LOG.info(f'Start evaluation on test dataset .....')
+        LOG.info('Start evaluation on test dataset .....')
 
-        LOG.info(f"..... validating test data")
-        
+        LOG.info("..... validating test data")
+
         ## schema checks
         try:
             validate = DataLoader().validate_schema(self.test_dataset)
             if validate is None:
-                LOG.info(f"PASS: Test data validation passed.")
+                LOG.info("PASS: Test data validation passed.")
         except:
-            raise Exception(f"ERROR - FAIL:(model_evaluation) - invalid input schema.")
+            raise Exception("ERROR - FAIL:(model_evaluation) - invalid input schema.")
 
         ## input checks
         if isinstance(self.test_dataset,dict):
@@ -120,46 +129,40 @@ class ModelName(BaseModel):
         data_features = sorted(self.dataset.columns.drop(['y']).tolist())
         if test_features != data_features:
             print(f"test features: {','.join(test_features)}")
-            raise Exception("ERROR - FAIL:(model_evaluation) - invalid features present") 
-        
+            raise Exception("ERROR - FAIL:(model_evaluation) - invalid features present")
+
         model_pickle_name = self.model_name + '_' + self.model_folder + '.' + self.model_version + '.pickle'
-        saved_model = os.path.join('models', 'saved_models', self.model_name, self.model_folder, model_pickle_name)  
+        saved_model = os.path.join('models', 'saved_models', self.model_name, self.model_folder, model_pickle_name)
 
         LOG.info(f"..... loading model {saved_model}")
 
         if not os.path.exists(saved_model):
             exc = (f"model '{saved_model}' cannot be found. Did you train the full model?")
             raise Exception(exc)
-    
+
         model = pickle.load(open(saved_model, 'rb'))
 
         ## make prediction and gather data for log entry
         y_pred = model.predict(self.X_test)
 
-        LOG.info(f"..... starting model prediction on test data") 
+        LOG.info("..... starting model prediction on test data")
 
         predictions = [round(value) for value in y_pred]
 
-        LOG.info(f"Model evaluation completed") 
+        LOG.info("Model evaluation completed")
 
         # evaluate predictions using train_test split - quicker
         accuracy = accuracy_score(self.y_test, predictions)
-        print("Train-Test split accuracy: %.2f%%" % (accuracy * 100.0))
+        print(f"Train-Test split accuracy: {round(accuracy * 100.0,2)} %")
 
         # evaluate predictions using Kfold method - good for sets model has not seen
         kfold = KFold(n_splits=10, random_state=7, shuffle = True)
         results = cross_val_score(model, self.X_test, self.y_test, cv=kfold)
-        print("K-fold validation accuracy (std): %.2f%% (%.2f%%)" % (results.mean()*100, results.std()*100))
+        print(f"K-fold validation accuracy (std): {round(results.mean()*100,2)} % ({round(results.std()*100,2)} %)")
 
         # evaluate predictions using Stratified Kfold method - good for multiple classes or imbalanced dataset
         stratified_kfold = StratifiedKFold(n_splits=10, random_state=7, shuffle = True)
         s_results = cross_val_score(model, self.X_test, self.y_test, cv=stratified_kfold)
-        print("Stratified K-fold validation accuracy (std): %.2f%% (%.2f%%)" % (s_results.mean()*100, s_results.std()*100))
+        print(f"Stratified K-fold validation accuracy (std): {round(s_results.mean()*100,2)} % ({round(s_results.std()*100,2)} %)")
 
         return predictions
-
-
-# if __name__ == '__main__':
-
-#     m = ModelName(BaseModel)
-#     m.load_data()
