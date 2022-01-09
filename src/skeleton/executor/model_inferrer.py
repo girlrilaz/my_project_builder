3496246c9c87615095f8bc18f40bf971315ef39c
Nina Raymond <norfazlinahamdan@gmail.com>
2022-01-09 20:13:10 +1100

    Update

--- src/skeleton/executor/model_inferrer.py
+++ src/skeleton/executor/model_inferrer.py
@@ -7,10 +7,14 @@ sys.path.append('.')
 # from utils.visualize import display
 from utils.config import Config
 from configs.module.config import CFG
+from utils.dataloader import DataLoader
 
 class ModelInferrer:
     def __init__(self):
         self.config = Config.from_json(CFG)
+        self.numerical = self.config.data.numerical_att
+        self.categorical = self.config.data.categorical_att
+        self.target = self.config.train.target_att
         self.saved_path = './models/saved_models/XGBoost/1/XGBoost_1.0.0.pickle'
         self.model = pickle.load(open(self.saved_path, 'rb'))
         # self.predict = self.model.signatures["serving_default"]
@@ -20,40 +24,46 @@ class ModelInferrer:
 
         ## input checks
         if isinstance(query, dict):
-            query_df = pd.DataFrame(query)
+            query = pd.DataFrame(query)
         elif isinstance(query, pd.DataFrame):
             pass
         else:
-            raise Exception(f"ERROR - FAIL:(model_evaluation) - invalid input. {query.dtype()} was given")
+            raise Exception(f"ERROR - FAIL:(model_evaluation) - invalid input. {type(query)} was given")
+
+        query_df = DataLoader().feature_pipeline(self.numerical, self.categorical).fit(query).transform(query)
+
         return query_df
 
     def infer(self, query):
   
         query = self.preprocess(query)
+
         pred = self.model.predict(query)
-        # pred = pred.numpy().tolist()
+        pred = pred.tolist()
         return {'prediction_output':pred}
 
 
 if __name__ == '__main__':
 
-    query = {'age': [30],
-            'job': ["unemployed"],
-            "marital": ["married"],
-            "education": ["primary"],
-            "default": ["no"],
-            "balance": [1787],
-            "housing": ["no"],
-            "loan": ["no"],
-            "contact": ["cellular"],
-            "day": [19],
-            "month": ["oct"],
-            "duration": [79],
-            "campaign": [1],
-            "pdays": [-1],
-            "previous": [0],
-            "poutcome":["unknown"]
-    }
-    print(ModelInferrer().infer(query))
+    # query = {'age': [30, 33],
+    #         'job': ["unemployed", "services"],
+    #         "marital": ["married", "married"],
+    #         "education": ["primary", "secondary"],
+    #         "default": ["no", "no"],
+    #         "balance": [1787, 4789],
+    #         "housing": ["no", "yes"],
+    #         "loan": ["no", "yes"],
+    #         "contact": ["cellular", "cellular"],
+    #         "day": [19, 11],
+    #         "month": ["oct", "may"],
+    #         "duration": [79, 220],
+    #         "campaign": [1, 1],
+    #         "pdays": [-1, 339],
+    #         "previous": [0, 4],
+    #         "poutcome":["unknown", "failure"]
+    # }
 
-    # print(ModelInferrer().infer(query))
+    df = pd.read_csv("./data/raw/bank.csv", delimiter=';')
+    query = df.drop(['y'], axis=1)
+
+    print(ModelInferrer().infer(query))
