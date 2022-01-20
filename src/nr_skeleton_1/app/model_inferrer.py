ff5f8faba366ad6d46bf96951cf7d5cf64844ef8
Nina Raymond <norfazlinahamdan@gmail.com>
2022-01-20 19:31:41 +1100

    Update

--- src/nr_skeleton_1/app/model_inferrer.py
+++ src/nr_skeleton_1/app/model_inferrer.py
@@ -10,82 +10,84 @@ from utils.config import Config
 from configs.module.config import CFG
 from utils.dataloader import DataLoader
 
-class ModelInferrer:
-    def __init__(self):
-        self.config = Config.from_json(CFG)
-        self.numerical = self.config.data.numerical_att
-        self.categorical = self.config.data.categorical_att
-        self.target = self.config.train.target_att
-        self.saved_path = './models/saved_models/XGBoost/1/XGBoost_1.0.0.pickle'
-        self.model = pickle.load(open(self.saved_path, 'rb'))
+# TODO : This is the model_inferrer.py that you have built in evaluator folder. Copy and Paste in app file to isolate
 
-    def preprocess(self, query):
+# class ModelInferrer:
+#     def __init__(self):
+#         self.config = Config.from_json(CFG)
+#         self.numerical = self.config.data.numerical_att
+#         self.categorical = self.config.data.categorical_att
+#         self.target = self.config.train.target_att
+#         self.saved_path = './models/saved_models/XGBoost/1/XGBoost_1.0.0.pickle'
+#         self.model = pickle.load(open(self.saved_path, 'rb'))
 
-        ## input checks
-        if isinstance(query, dict):
-            query = pd.DataFrame(query)
-        elif isinstance(query, pd.DataFrame):
-            pass
-        else:
-            raise Exception(f"ERROR - FAIL:(model_evaluation) - invalid input. {type(query)} was given")
+#     def preprocess(self, query):
 
-        X = query.drop(['y'], axis=1)
-        y = query[['y']]
-        query_train = DataLoader().feature_pipeline(self.numerical, self.categorical).fit(X).transform(X) # get from model trainer log
+#         ## input checks
+#         if isinstance(query, dict):
+#             query = pd.DataFrame(query)
+#         elif isinstance(query, pd.DataFrame):
+#             pass
+#         else:
+#             raise Exception(f"ERROR - FAIL:(model_evaluation) - invalid input. {type(query)} was given")
+
+#         X = query.drop(['y'], axis=1)
+#         y = query[['y']]
+#         query_train = DataLoader().feature_pipeline(self.numerical, self.categorical).fit(X).transform(X) # get from model trainer log
    
-        for n_samples in range(10, len(X), 20):
+#         for n_samples in range(10, len(X), 20):
 
-            subset_indices = X.sample(n=n_samples, replace=True)
-            subset_query = DataLoader().feature_pipeline(self.numerical, self.categorical).fit(subset_indices).transform(subset_indices)
-            if subset_query.shape[1] != query_train.shape[1]:
-                continue
-            else:
-                    break
+#             subset_indices = X.sample(n=n_samples, replace=True)
+#             subset_query = DataLoader().feature_pipeline(self.numerical, self.categorical).fit(subset_indices).transform(subset_indices)
+#             if subset_query.shape[1] != query_train.shape[1]:
+#                 continue
+#             else:
+#                     break
 
-        # print(f'n_ : {n_samples}')
-        # print(f'y_ : {subset_query.shape[1]}')
+#         # print(f'n_ : {n_samples}')
+#         # print(f'y_ : {subset_query.shape[1]}')
 
-        return subset_query, subset_indices, y
+#         return subset_query, subset_indices, y
 
-    def infer(self, query):
+#     def infer(self, query):
   
-        subset_query, subset_indices, y = self.preprocess(query)
-        pred = self.model.predict(subset_query)
-        pred = pred.tolist()
+#         subset_query, subset_indices, y = self.preprocess(query)
+#         pred = self.model.predict(subset_query)
+#         pred = pred.tolist()
 
-        # output in dictionary
-        #prediction_dict = {'variable_input': subset_indices, 'actual_class': y, 'prediction_output':pred}
+#         # output in dictionary
+#         #prediction_dict = {'variable_input': subset_indices, 'actual_class': y, 'prediction_output':pred}
 
-        # output in datadrame
-        prediction_df = subset_indices
-        prediction_df['actual_y'] = y
-        prediction_df['pred_y'] = pred
-        prediction_df['predicted_y'] = np.where(prediction_df['pred_y']==0, 'no', 'yes')
-        prediction_df.drop('pred_y', axis = 1, inplace=True)
+#         # output in datadrame
+#         prediction_df = subset_indices
+#         prediction_df['actual_y'] = y
+#         prediction_df['pred_y'] = pred
+#         prediction_df['predicted_y'] = np.where(prediction_df['pred_y']==0, 'no', 'yes')
+#         prediction_df.drop('pred_y', axis = 1, inplace=True)
 
-        return prediction_df
+#         return prediction_df
 
 
-if __name__ == '__main__':
+# if __name__ == '__main__':
 
-    # query = {'age': [30, 33],
-    #         'job': ["unemployed", "services"],
-    #         "marital": ["married", "married"],
-    #         "education": ["primary", "secondary"],
-    #         "default": ["no", "no"],
-    #         "balance": [1787, 4789],
-    #         "housing": ["no", "yes"],
-    #         "loan": ["no", "yes"],
-    #         "contact": ["cellular", "cellular"],
-    #         "day": [19, 11],
-    #         "month": ["oct", "may"],
-    #         "duration": [79, 220],
-    #         "campaign": [1, 1],
-    #         "pdays": [-1, 339],
-    #         "previous": [0, 4],
-    #         "poutcome":["unknown", "failure"]
-    # }
+#     # query = {'age': [30, 33],
+#     #         'job': ["unemployed", "services"],
+#     #         "marital": ["married", "married"],
+#     #         "education": ["primary", "secondary"],
+#     #         "default": ["no", "no"],
+#     #         "balance": [1787, 4789],
+#     #         "housing": ["no", "yes"],
+#     #         "loan": ["no", "yes"],
+#     #         "contact": ["cellular", "cellular"],
+#     #         "day": [19, 11],
+#     #         "month": ["oct", "may"],
+#     #         "duration": [79, 220],
+#     #         "campaign": [1, 1],
+#     #         "pdays": [-1, 339],
+#     #         "previous": [0, 4],
+#     #         "poutcome":["unknown", "failure"]
+#     # }
 
-    df = pd.read_csv("./data/raw/bank.csv", delimiter=';')
+#     df = pd.read_csv("./data/raw/bank.csv", delimiter=';')
 
-    print(ModelInferrer().infer(df))
+#     print(ModelInferrer().infer(df))
